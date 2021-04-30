8abf3037cc3e9dbcf06c4a7370b834c0527c3f50
muzhenpeng <zhiwei741@icloud.com>
2021-04-30 15:45:17 +0800

    增加rocketmq

---
+++ rocketmq/src/test/java/com/golike/rocketmq/RocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golike.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
