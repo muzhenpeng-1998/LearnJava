74ad6cb04cfe27d6fbb579a75f45685faeefe6b0
muzhenpeng <zhiwei741@icloud.com>
2021-04-30 15:42:28 +0800

    增加springboot

---
+++ springboot/src/test/java/com/golike/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golike.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
