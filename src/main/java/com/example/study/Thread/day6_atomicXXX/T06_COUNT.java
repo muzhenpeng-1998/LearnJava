535191bf984f7d569e6fe006fa69d16e37f36e9a
muzhenpeng <zhiwei741@icloud.com>
2021-05-16 07:18:37 +0800

    增加spring

---
+++ src/main/java/com/example/study/Thread/day6_atomicXXX/T06_COUNT.java
@@ -0,0 +1,36 @@
+package com.example.study.Thread.day6_atomicXXX;
+
+import java.util.concurrent.CountDownLatch;
+
+public class T06_COUNT {
+    public static void main(String[] args) {
+//        usingJoin();
+        usingCountDownLatch();
+    }
+
+    private static void usingCountDownLatch() {
+        Thread[] threads = new Thread[100];
+        CountDownLatch latch = new CountDownLatch(threads.length);
+
+        for(int i=0; i<threads.length; i++) {
+            threads[i] = new Thread(()->{
+                int result = 0;
+                for(int j=0; j<10000; j++) result += j;
+                latch.countDown();
+            });
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("end latch");
+    }
+
+}
